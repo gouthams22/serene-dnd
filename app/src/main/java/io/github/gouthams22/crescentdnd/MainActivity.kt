ab03c26bd652604dbe386e72193b24e50f164b79
Goutham Subramani <gouthams0922@gmail.com>
2022-10-24 00:39:53 -0500

    +added some colors

--- app/src/main/java/io/github/gouthams22/crescentdnd/MainActivity.kt
+++ app/src/main/java/io/github/gouthams22/crescentdnd/MainActivity.kt
@@ -1,11 +1,14 @@
 package io.github.gouthams22.crescentdnd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
     }
 }
